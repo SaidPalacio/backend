2e7dd6605d8347eb413ea0692039d75c5f0c624a
Said Palacio <saidpalacio10@gmail.com>
2024-05-18 10:10:07 -0500

    db arreglada

--- config/db.py
+++ config/db.py
@@ -4,11 +4,11 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/tasksul'
-user = "winsignares"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/sillas'
+user = "said10"
 password = "admin123"
-direc = "winsignares.mysql.pythonanywhere-services.com"
-namebd = "winsignares$tasksul"
+direc = "said10.mysql.pythonanywhere-services.com"
+namebd = "said10$sillas"
 #app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{direc}/{namebd}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "Movil2"
