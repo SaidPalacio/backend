117cc013179246fd7aef1823af2f392f807d447d
cristian Hernández vergara <cristian0137>
2024-05-26 12:58:48 -0500

    cambio

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, redirect, render_template, jsonify
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from config.db import app, db
-
+from flask_cors import CORS
 # Trabajar en las rutas de blueprint con respecto a las API's
 from api.UsuariosApi import ruta_user
 from api.ProveedorApi import ruta_provee
@@ -9,12 +9,11 @@ from api.SillasApi import ruta_silla
 from api.CategoriasApi import ruta_categoria
 from api.ReservaApi import ruta_reserva
 from api.PagoApi import ruta_pago
-from common.Token import generar_token, verificar_token
 
 # Configuración de JWT
 app.config['JWT_SECRET_KEY'] = 'abcde'
 jwt = JWTManager(app)
-
+CORS(app)
 # Importar los Blueprints
 app.register_blueprint(ruta_user, url_prefix="/api")
 app.register_blueprint(ruta_provee, url_prefix="/api")
@@ -22,26 +21,6 @@ app.register_blueprint(ruta_silla, url_prefix="/api")
 app.register_blueprint(ruta_categoria, url_prefix="/api")
 app.register_blueprint(ruta_reserva, url_prefix="/api")
 app.register_blueprint(ruta_pago, url_prefix="/api")
-
-# Ruta para obtener el token
-@app.route('/obtenertoken', methods=["GET"])
-def obtenertoken():
-    datatoken = generar_token('Said', '123')
-    var_token = datatoken['token']
-    response = {
-        "statusCode": 200,
-        "body": var_token
-    }
-    return jsonify(response)
-
-# Ruta para verificar el token
-@app.route('/verificaciontoken', methods=["GET"])
-def verificaciontoken():
-    token = request.headers['Authorization']
-    token = token.replace('Bearer ', "").replace("", "")
-    vf = verificar_token(token)
-    print(f"vf=>{vf}")
-    return jsonify(vf)
 
 # Configurar el servidor
 @app.route("/")
--- config/db.py
+++ config/db.py
@@ -4,12 +4,12 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/AlquilerSillas'
-user = "cristian"
-password = "admin123"
-direc = "cristian.mysql.pythonanywhere-services.com"
-namebd = "cristian$AlquilerSillas"
-#app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{direc}/{namebd}'
+##app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/AlquilerSillas'
+user = "cristian8261"
+password = "admin123456"
+direc = "cristian8261.mysql.pythonanywhere-services.com"
+namebd = "cristian8261$AlquilerSillas"
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{direc}/{namebd}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "Alquiler"
 
